3ab370ab489960bfab9ead9bef2f8ff216e9b8e5
gvellos <99793937+gvellos@users.noreply.github.com>
2023-01-18 22:25:44 +0200

    Update Form1.cs

--- battleship/battleship/Form1.cs
+++ battleship/battleship/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,7 +23,6 @@ namespace battleship
         private void Form1_Load(object sender, EventArgs e)
         {
             Random random = new Random();
-            Random randomxy = new Random();
             List<string> userShips = new List<string>();
             List<string> botShips = new List<string>();
             userShips.Add("Aeroplanoforo");
@@ -50,6 +49,8 @@ namespace battleship
 
             for (int y = 0; y < 4; y++)
             {
+                bool flag = true;
+                bool flagy = true;
                 int tempxy = random.Next(0, 2);
                 int temp = random.Next(0, 99);
                 int tempx = temp / 10;
@@ -104,26 +105,61 @@ namespace battleship
                             userArray[tempx, tempy + 4] = 1;
                         }
                     }
-                    // userShips.RemoveAt(0);
+                    userShips.RemoveAt(0);
+                    tempxy = random.Next(0, 2);
+                    temp = random.Next(0, 99);
+                    tempx = temp / 10;
+                    tempy = temp % 10;
                     if (userShips[0] == "Antitorpiliko")
                     {
                         if (tempxy == 0)
                         {
-                            if (tempx < 7)
+                            int pos = Line(userArray, tempx, 4) - 4;
+                             flag = pos >=0 && pos + 4 <= 6;
+                            if (flag)// to chabge it if neccesery
                             {
-                                if (Line(userArray, tempx))
-                                {
-
-                                }
-                                if (userArray[tempx, tempy] != 1 && userArray[tempx + 1, tempy] != 1 && userArray[tempx + 2, tempy] != 1 && userArray[tempx + 3, tempy] != 1)
-                                {
-                                    userArray[tempx, tempy] = 1;
-                                    userArray[tempx + 1, tempy] = 1;
-                                    userArray[tempx + 2, tempy] = 1;
-                                    userArray[tempx + 3, tempy] = 1;
-                                }
+                                userArray[pos, tempy] = 1;
+                                userArray[pos + 1, tempy] = 1;
+                                userArray[pos + 2, tempy] = 1;
+                                userArray[pos + 3, tempy] = 1;
                             }
+                            else
+                            {
+                                do
+                                {
+                                    temp = random.Next(0, 99);
+                                    tempx = temp / 10;
+                                } while (Line(userArray, tempx, 4) != -1);
+                                userArray[pos, tempy] = 1;
+                                userArray[pos + 1, tempy] = 1;
+                                userArray[pos + 2, tempy] = 1;
+                                userArray[pos + 3, tempy] = 1;
+                            }
+                        }
+                        else
+                        {
+                            int posy = Liney(userArray, tempy, 4) - 4;
+                             flagy = (posy >=0) && (posy + 4 <= 6);
+                            if (flagy) // to chabge it if neccesery
+                            {
+                                userArray[tempx, posy] = 1;
+                                userArray[tempx, posy + 1] = 1;
+                                userArray[tempx, posy + 2] = 1;
+                                userArray[tempx, posy + 3] = 1;
 
+                            }
+                            else
+                            {
+                                do
+                                {
+                                    temp = random.Next(0, 99);
+                                    tempy = temp % 10;
+                                } while (Liney(userArray, tempy, 4) != -1);
+                                userArray[tempx, posy] = 1;
+                                userArray[tempx, posy + 1] = 1;
+                                userArray[tempx, posy + 2] = 1;
+                                userArray[tempx, posy + 3] = 1;
+                            }
                         }
                     }
                 }
@@ -153,6 +189,15 @@ namespace battleship
                 locx = locx - 300;
             }
 
+            for (int i = 0; i < 10; i++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    Console.WriteLine(userArray[i, y]);
+                }
+                Console.WriteLine("\n");
+            }
+
 
 
 
@@ -180,11 +225,14 @@ namespace battleship
             }
         }
 
-        public bool Line(int[,] arr, int line)
+        public int Line(int[,] arr, int line ,int position)
         {
             int counter = 0;
-            int temp = 0;
-            for (int y = 0; y < 10; y++)
+            int max = 0;
+            int y = 0;
+            int pos = 0;
+
+            for ( y = 0; y < 10; y++)
             {
                 if (arr[y,line] == 0)
                 {
@@ -192,11 +240,71 @@ namespace battleship
                 }
                 else
                 {
-                    temp = counter;
+
+                    if (counter>max)
+                    {
+                        max = counter;
+                        pos = y;
+                    }
                     counter = 0;
                 }
             }
-        return true;
+
+            if (counter > max)
+            {
+                max = counter;
+                pos = y;
+            }
+            if(max >= position)
+            {
+                return pos;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+
+
+        public int Liney(int[,] arr, int line, int position)
+        {
+            int counter = 0;
+            int max = 0;
+            int y = 0;
+            int pos = 0;
+
+            for (y = 0; y < 10; y++)
+            {
+                if (arr[line, y] == 0)
+                {
+                    counter++;
+                }
+                else
+                {
+
+                    if (counter > max)
+                    {
+                        max = counter;
+                        pos = y;
+                    }
+                    counter = 0;
+                }
+            }
+
+            if (counter > max)
+            {
+                max = counter;
+                pos = y;
+            }
+            if (max >= position)
+            {
+                return pos;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
