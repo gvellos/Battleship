620bc6456cab24f993d1514bb725f3eee7abc575
gvellos <99793937+gvellos@users.noreply.github.com>
2023-02-05 16:18:35 +0200

    Update Form1.cs

--- battleship/battleship/Form1.cs
+++ battleship/battleship/Form1.cs
@@ -10,11 +10,22 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace battleship
 {
     public partial class Form1 : Form
     {
+        string nameb;
+        string name;
+
+        int[,] userArray = new int[10, 10];
+        int[,] botArray = new int[10, 10];
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        List<PictureBox> pictureBoxesUser = new List<PictureBox>();
+        List<string> randCordinates = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +33,7 @@ namespace battleship
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
             Random random = new Random();
             List<string> userShips = new List<string>();
             List<string> botShips = new List<string>();
@@ -37,264 +49,227 @@ namespace battleship
 
             int locx = 100;
             int locy = 100;
-            string name = "1";
-            int[,] userArray = new int[10, 10];
+            int locbx = 500;
+            int locby = 100;
+
+            
+            
             for (int i = 0; i < 10; i++)
             {
                 for (int y = 0; y < 10; y++)
                 {
                     userArray[i, y] = 0;
+                    botArray[i, y] = 0;
                 }
             }
 
-            int tempxy = random.Next(0, 2);
-            int tempx = random.Next(0, 10);
-            int tempy = random.Next(0, 10);
-            if (userShips[0] == "Aeroplanoforo")
-            { 
-        
-                if (tempxy == 0)
-                {
-                    if (tempx < 6)
-                    {
-                        userArray[tempx, tempy] = 1;
-                        userArray[tempx + 1, tempy] = 1;
-                        userArray[tempx + 2, tempy] = 1;
-                        userArray[tempx + 3, tempy] = 1;
-                        userArray[tempx + 4, tempy] = 1;
-                    }
-                    else
-                    {
-                        do
-                        {
-                            tempx = random.Next(0, 10);
-                            tempy = random.Next(0, 10);
-                        } while (tempx >= 6);
-                        userArray[tempx, tempy] = 1;
-                        userArray[tempx + 1, tempy] = 1;
-                        userArray[tempx + 2, tempy] = 1;
-                        userArray[tempx + 3, tempy] = 1;
-                        userArray[tempx + 4, tempy] = 1;
-                    }
-                }
-                else
-                {
-                    if (tempy < 6)
-                    {
-                        userArray[tempx, tempy] = 1;
-                        userArray[tempx, tempy + 1] = 1;
-                        userArray[tempx, tempy + 2] = 1;
-                        userArray[tempx, tempy + 3] = 1;
-                        userArray[tempx, tempy + 4] = 1;
-                    }
-                    else
-                    {
-                        do
-                        {
-                            tempx = random.Next(0, 10);
-                            tempy = random.Next(0, 10);
-                        } while (tempy >= 6);
-                        userArray[tempx, tempy] = 1;
-                        userArray[tempx, tempy + 1] = 1;
-                        userArray[tempx, tempy + 2] = 1;
-                        userArray[tempx, tempy + 3] = 1;
-                        userArray[tempx, tempy + 4] = 1;
-                    }
-                }
-            }
-
-            if (userShips[1] == "Antitorpiliko")
-            {
-                 tempx = random.Next(0, 10);
-                 tempy = random.Next(0, 10);
-                 tempxy = random.Next(0, 2);
-                if (tempxy == 0)
-                {
-                    while (tempx > 6 && (userArray[tempx, tempy] == 1 && userArray[tempx + 1, tempy] == 1 && userArray[tempx + 2, tempy] == 1 && userArray[tempx + 3, tempy] == 1))
-                    {
-                        tempx = random.Next(0, 10);
-                        tempy = random.Next(0, 10);
-                    }
-                    userArray[tempx, tempy] = 1;
-                    userArray[tempx + 1, tempy] = 1;
-                    userArray[tempx + 2, tempy] = 1;
-                    userArray[tempx + 3, tempy] = 1;
-                }
-                else
-                {
-                    while (tempy > 6 && (userArray[tempx, tempy] == 1 && userArray[tempx, tempy + 1] == 1 && userArray[tempx, tempy + 2] == 1 && userArray[tempx, tempy + 3] == 1) )
-                    {
-                        tempx = random.Next(0, 10);
-                        tempy = random.Next(0, 10);
-                        
-                    }
-                    userArray[tempx, tempy] = 1;
-                    userArray[tempx, tempy + 1] = 1;
-                    userArray[tempx, tempy + 2] = 1;
-                    userArray[tempx, tempy + 3] = 1;
-                }
-            }
-
-            if (userShips[2] == "Polemiko")
-            {
-                tempx = random.Next(0, 10);
-                tempy = random.Next(0, 10);
-                tempxy = random.Next(0, 2);
-                if (tempxy == 0)
-                {
-                    while (tempx > 7 && (userArray[tempx, tempy] == 1 && userArray[tempx + 1, tempy] == 1 && userArray[tempx + 2, tempy] == 1 ))
-                    {
-                        tempx = random.Next(0, 10);
-                        tempy = random.Next(0, 10);
-                    }
-                    userArray[tempx, tempy] = 1;
-                    userArray[tempx + 1, tempy] = 1;
-                    userArray[tempx + 2, tempy] = 1;
-                }
-                else
-                {
-                    while (tempy > 6 && (userArray[tempx, tempy] == 1 && userArray[tempx, tempy + 1] == 1 && userArray[tempx, tempy + 2] == 1 ))
-                    {
-                        tempx = random.Next(0, 10);
-                        tempy = random.Next(0, 10);
-
-                    }
-                    userArray[tempx, tempy] = 1;
-                    userArray[tempx, tempy + 1] = 1;
-                    userArray[tempx, tempy + 2] = 1;
-                }
-            }
-
-
+            insertShip(userArray, userShips, random);
+            insertShip(botArray, botShips, random);
 
             for (int i = 0; i < 10; i++)
             {
                 for (int y = 0; y < 10; y++)
                 {
-
+                    name = $"{i},{y}";
                     PictureBox picturebox1 = new PictureBox();
-                    picturebox1.Name = $"picturebox{name}";
+                    picturebox1.Name = $"{name}";
                     picturebox1.Location = new Point(locx, locy);
                     picturebox1.Size = new Size(25, 25);
                     picturebox1.BackColor = Color.Black;
                     picturebox1.ForeColor = Color.GhostWhite;
                     Controls.Add(picturebox1);
-                    name = $"{i}{y}";
+                    randCordinates.Add(name);
+                    pictureBoxesUser.Add(picturebox1);
                     locx = locx + 30;
                     if (userArray[i, y] == 1)
                     {
                         picturebox1.BackColor = Color.Gray;
-                    }
+                    } 
                 }
                 locy = locy + 30;
                 locx = locx - 300;
             }
 
+            var result = randCordinates.OrderBy(item => random.Next()).ToList();
+            for(int i = 0; i < result.Count; i++)
+            {
+                randCordinates[i] = result[i];
+            }
 
-           int locbx = 500;
-            int locby = 100;
-            string nameb = "1";
-            int[,] botArray = new int[10, 10];
             for (int i = 0; i < 10; i++)
             {
                 for (int y = 0; y < 10; y++)
                 {
-                    botArray[i, y] = 0;
+                    nameb = $"{i},{y}";                    
                     PictureBox picturebox1 = new PictureBox();
-                    picturebox1.Name = $"picturebox{nameb}";
+                    picturebox1.Name = $"{nameb}";
                     picturebox1.Location = new Point(locbx, locby);
                     picturebox1.Size = new Size(25, 25);
                     picturebox1.BackColor = Color.Black;
                     picturebox1.ForeColor = Color.GhostWhite;
+                    picturebox1.MouseDown += Picturebox1_MouseDown;
+                    pictureBoxes.Add(picturebox1);
                     Controls.Add(picturebox1);
-                    nameb = $"{i}{y}";
                     locbx = locbx + 30;
                 }
                 locby = locby + 30;
                 locbx = locbx - 300;
             }
         }
-
-        public int Line(int[,] arr, int line ,int position)
+        void Picturebox1_MouseDown(object sender, EventArgs e)
         {
-            int counter = 0;
-            int max = 0;
-            int y = 0;
-            int pos = 0;
-
-            for ( y = 0; y < 10; y++)
+            if (sender is PictureBox)
             {
-                if (arr[y,line] == 0)
+                string name  = ((PictureBox)sender).Name;
+                string[] result = name.Split(',');
+                int x = Int32.Parse(result[0]);
+                int y = Int32.Parse(result[1]);
+                foreach(PictureBox picturebox in pictureBoxes)
                 {
-                    counter++;
+                    if(picturebox.Name == name)
+                    {
+                        if (botArray[x,y] == 1)
+                        {
+                            picturebox.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            picturebox.BackColor = Color.Red;
+                        }
+                        string resultBotForUser = randCordinates[0];
+                        string[] resultBotCordinates = resultBotForUser.Split(',');
+                        int xBot = Int32.Parse(resultBotCordinates[0]);
+                        int yBot = Int32.Parse(resultBotCordinates[1]);
+                        randCordinates.RemoveAt(0);
+
+
+                        
+                        PictureBox tempPictureBox = pictureBoxesUser[xBot * 10 + yBot];
+                        if (userArray[xBot, yBot] == 1)
+                        {
+                            tempPictureBox.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            tempPictureBox.BackColor = Color.Red;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void insertShip(int[,] shipArray , List<string> shipNames, Random random)
+        {
+           
+            int tempxy = random.Next(0, 2);
+            int tempx = random.Next(0, 6); //orizontia
+            int tempy = random.Next(0, 6); // katheta
+            if (shipNames[0] == "Aeroplanoforo")
+            {
+                if (tempxy == 0) // θα ειναι καθετα τα πλοια
+                {
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx + 1, tempy] = 1;
+                    shipArray[tempx + 2, tempy] = 1;
+                    shipArray[tempx + 3, tempy] = 1;
+                    shipArray[tempx + 4, tempy] = 1;
                 }
                 else
                 {
-
-                    if (counter>max)
-                    {
-                        max = counter;
-                        pos = y;
-                    }
-                    counter = 0;
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx, tempy + 1] = 1;
+                    shipArray[tempx, tempy + 2] = 1;
+                    shipArray[tempx, tempy + 3] = 1;
+                    shipArray[tempx, tempy + 4] = 1;
                 }
             }
 
-            if (counter > max)
+            if (shipNames[1] == "Antitorpiliko")
             {
-                max = counter;
-                pos = y;
+                tempx = random.Next(0, 7); // orizontia
+                tempy = random.Next(0, 7);
+                tempxy = random.Next(0, 2);
+                if (tempxy == 0) // το πλοιο θα ειναι καθετα
+                {
+                    while (shipArray[tempx, tempy] == 1 || shipArray[tempx + 1, tempy] == 1 || shipArray[tempx + 2, tempy] == 1 || shipArray[tempx + 3, tempy] == 1)
+                    {
+                        tempx = random.Next(0, 7); // orizontia
+                    }
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx + 1, tempy] = 1;
+                    shipArray[tempx + 2, tempy] = 1;
+                    shipArray[tempx + 3, tempy] = 1;
+                }
+                else
+                {
+                    while (shipArray[tempx, tempy] == 1 || shipArray[tempx, tempy + 1] == 1 || shipArray[tempx, tempy + 2] == 1 || shipArray[tempx, tempy + 3] == 1)
+                    {
+                        tempy = random.Next(0, 7);
+                    }
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx, tempy + 1] = 1;
+                    shipArray[tempx, tempy + 2] = 1;
+                    shipArray[tempx, tempy + 3] = 1;
+                }
             }
-            if(max >= position)
+
+            if (shipNames[2] == "Polemiko")
             {
-                return pos;
+                tempx = random.Next(0, 8);
+                tempy = random.Next(0, 8);
+                tempxy = random.Next(0, 2);
+                if (tempxy == 0)// το πλοιο θα ειναι καθετα
+                {
+                    while (shipArray[tempx, tempy] == 1 || shipArray[tempx + 1, tempy] == 1 || shipArray[tempx + 2, tempy] == 1)
+                    {
+                        tempx = random.Next(0, 8); // orizontia
+                    }
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx + 1, tempy] = 1;
+                    shipArray[tempx + 2, tempy] = 1;
+                }
+                else // το πλοιο θα ειναι orizontia
+                {
+                    while (shipArray[tempx, tempy] == 1 || shipArray[tempx, tempy + 1] == 1 || shipArray[tempx, tempy + 2] == 1)
+                    {
+                        tempy = random.Next(0, 8);
+                    }
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx, tempy + 1] = 1;
+                    shipArray[tempx, tempy + 2] = 1;
+                }
             }
-            else
+
+            if (shipNames[3] == "Ypovrixio")
             {
-                return -1;
+                tempx = random.Next(0, 9);
+                tempy = random.Next(0, 9);
+                tempxy = random.Next(0, 2);
+                if (tempxy == 0)// το πλοιο θα ειναι καθετα
+                {
+                    while (shipArray[tempx, tempy] == 1 || shipArray[tempx + 1, tempy] == 1)
+                    {
+                        tempx = random.Next(0, 9); // orizontia
+                    }
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx + 1, tempy] = 1;
+                }
+                else // το πλοιο θα ειναι orizontia
+                {
+                    while (shipArray[tempx, tempy] == 1 || shipArray[tempx, tempy + 1] == 1)
+                    {
+                        tempy = random.Next(0, 9);
+                    }
+                    shipArray[tempx, tempy] = 1;
+                    shipArray[tempx, tempy + 1] = 1;
+                }
             }
         }
 
 
 
-        public int Liney(int[,] arr, int line, int position)
-        {
-            int counter = 0;
-            int max = 0;
-            int y = 0;
-            int pos = 0;
-
-            for (y = 0; y < 10; y++)
-            {
-                if (arr[line, y] == 0)
-                {
-                    counter++;
-                }
-                else
-                {
-
-                    if (counter > max)
-                    {
-                        max = counter;
-                        pos = y;
-                    }
-                    counter = 0;
-                }
-            }
-
-            if (counter > max)
-            {
-                max = counter;
-                pos = y;
-            }
-            if (max >= position)
-            {
-                return pos;
-            }
-            else
-            {
-                return -1;
-            }
-        }
+       
+        
     }
 }
